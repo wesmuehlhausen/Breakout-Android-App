be3616132d8a0c51dcdf0b012fcce909886ef02c
Aaron Miller <amiller26@zagmail.gonzaga.edu>
2021-12-15 01:26:50 -0800

    sfsf:

--- app/src/main/java/com/example/breakbrickerdevolution/GameRules.java
+++ app/src/main/java/com/example/breakbrickerdevolution/GameRules.java
@@ -10,17 +10,6 @@ import android.graphics.Point;
 import android.graphics.RectF;
 import android.view.SurfaceHolder;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.util.Scanner;
 
 public class GameRules {
 
